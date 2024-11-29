eff293e10412d7b411feb74e7c155ef54b6af0d0
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-11-29 07:46:46 -0500

    active link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,10 +8,7 @@ import { useState, useEffect } from 'react'; // Import hooks
 export default function Home() {
   const pathname = usePathname(); // Get current path
   const [isMobile, setIsMobile] = useState(false); // State to track mobile view
-  const [activeLink, setActiveLink] = useState("/");
-  const handleLinkClick = (path: string): void => {
-    setActiveLink(path);
-  };
+
   // Effect to handle screen width
   useEffect(() => {
     const updateScreenSize = () => {
@@ -24,18 +21,17 @@ export default function Home() {
 
   return (
     <div className="home-container">
-
       <header className="header">
         <Link href="/" className="logo-link">
-          <Image src="/elifcakmaklogo.svg" alt="Elif Cakmak Logo" width={100} height={100} className="logo" />
+          <Image src="/elifcakmaklogo.svg" alt="Elif Cakmak Logo" width={100} height={100} className="logo"/>
         </Link>
         <nav className="nav">
           <Link href="/" className={`nav-link ${pathname === '/' ? 'active' : ''}`}>Home</Link>
-          <Link href="/about" className={`nav-link ${pathname === '/about' ? 'active' : ''}`}>About</Link>
-          <Link href="/podcast" className={`nav-link ${pathname === '/podcast' ? 'active' : ''}`}>Podcast</Link>
-          <Link href="/apps" className={`nav-link ${pathname === '/apps' ? 'active' : ''}`}>Apps</Link>
-          <Link href="/books" className={`nav-link ${pathname === '/books' ? 'active' : ''}`}>Books</Link>
-          <Link href="/newsletter" className="newsletter-button" onClick={() => handleLinkClick('/newsletter')}>Newsletter</Link>
+          <Link href="/about"className={`nav-link ${pathname === '/about' ? 'active' : ''}`}>About</Link>
+          <Link href="/podcast"className={`nav-link ${pathname === '/podcast' ? 'active' : ''}`}>Podcast</Link>
+          <Link href="/apps"className={`nav-link ${pathname === '/apps' ? 'active' : ''}`}>Apps</Link>
+          <Link href="/books"className={`nav-link ${pathname === '/books' ? 'active' : ''}`}>Books</Link>
+          <Link href="/newsletter" className={`newsletter-button ${pathname === '/newsletter' ? 'active' : ''}`}>Newsletter</Link>
         </nav>
       </header>
 
