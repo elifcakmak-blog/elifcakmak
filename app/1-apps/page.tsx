d36de0ca6c183bcec29aa51421f1d54af330e85b
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-11-30 17:36:42 -0500

    usepathname

--- app/1-apps/page.tsx
+++ app/1-apps/page.tsx
@@ -1,16 +1,12 @@
 "use client"; // Add this directive to make this a client component
 
-import { usePathname } from 'next/navigation'; // Import to get the current path
 import UnderConstruction from '../3-UnderConstruction/UnderConstruction'; // Import the new component
 import Navigation from '../0-navigation/navigation';
 
 export default function Home() {
-  const pathname = usePathname(); // Get current path
-
   return (
     <div className="home-container">
       {/* Import Navigation */}
-
       <Navigation /> 
 
       {/* Use the UnderConstruction component */}
@@ -23,3 +19,4 @@ export default function Home() {
     </div>
   );
 }
+
