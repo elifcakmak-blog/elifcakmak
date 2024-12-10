a7dc0674ffb7b1839a50f5b6e2c97fe758e2c273
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-10 16:52:53 -0500

    final structure check

--- app/5/about-services/page.tsx
+++ app/5/about-services/page.tsx
@@ -16,43 +16,43 @@ const AbServices2 = () => {
       </div>
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">Podcasts</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">Podcasts</h3>
             <p className="mt-2 text-gray-600">Simplified Insights on Data and Software</p>
             <Link href="/2/podcast">
               <button className="btn-primary mt-4">Listen</button>
             </Link>
           </div>
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">Apps</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">Apps</h3>
             <p className="mt-2 text-gray-600">Conquer Tasks and Learning Objectives</p>
             <Link href="/2/apps">
               <button className="btn-primary mt-4">Download</button>
             </Link>
           </div>
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">GitHub</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">GitHub</h3>
             <p className="mt-2 text-gray-600">Explore my Current Software Development Projects</p>
             <Link href="https://github.com/elifcakmak-blog">
               <button className="btn-primary mt-4">Explore</button>
             </Link>
           </div>
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">Roadmap</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">Roadmap</h3>
             <p className="mt-2 text-gray-600">See Upcoming Updates and Milestones I&apos;m Completing</p>
             <Link href="/3">
               <button className="btn-primary mt-4">Observe</button>
             </Link>
           </div>
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">Videos</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">Videos</h3>
             <p className="mt-2 text-gray-600">Tutorials That Upgrade Your Skill Set</p>
             <Link href="/2/videos">
               <button className="btn-primary mt-4">Watch</button>
             </Link>
           </div>
-          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[300px]">
-            <h3 className="text-xl font-bold">Courses</h3>
+          <div className="service-card p-8 bg-[#1c0e45] shadow-md rounded-md text-center hover:shadow-lg transform hover:translateY(-4px) transition-transform h-[210px]">
+            <h3 className="text-lg font-bold">Courses</h3>
             <p className="mt-2 text-gray-600">Learn, Grow, and Achieve Mastery</p>
             <Link href="https://www.effortlessworks.elifcakmak.blog/courses" target="_blank" rel="noopener noreferrer">
               <button className="btn-primary mt-4">Learn</button>
