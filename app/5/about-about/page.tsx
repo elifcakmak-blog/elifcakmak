63becacbbb9237df409c4a39a40032ba6aab0187
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-12 13:28:22 -0500

    updated about page for mobile view

--- app/5/about-about/page.tsx
+++ app/5/about-about/page.tsx
@@ -5,16 +5,16 @@ const AbSection2 = () => {
   return (
     <section className="ab-section2 py-8 mb-8">
       <div className="ab-content2 px-8 ml-0">
-  <p className="mt-4 text-3lg text-hover-yellow">
+  <p className="mt-4 ab-content2 text-hover-yellow">
   I&apos;m the founder of Effortless Works and Easy Flow Media, dedicated to simplifying workflows so people can achieve their goals with ease and efficiency.
   </p>
-  <p className="mt-4 text-3lg text-hover-yellow">
+  <p className="mt-4 ab-content2 text-hover-yellow">
   In 2024, I began sharing insights on streamlining operations and designing tools that make work effortless. Drawing from my experience as an operations assistant for a leading financial consulting firm, I&apos;ve honed my ability to create systems that drive clarity and productivity—only now, I do it even better.
   </p>
-  <p className="mt-4 text-3lg text-hover-yellow">
+  <p className="mt-4 ab-content2 text-hover-yellow">
   Today, I&apos;m recognized as a leading expert in data management. Through my podcasts, apps, books, and digital product lines, I empower businesses and individuals with the tools and strategies they need to eliminate chaos and focus on what truly matters.
   </p>
-  <p className="mt-4 text-3lg text-hover-yellow">
+  <p className="mt-4 ab-content2 text-hover-yellow">
   I&apos;m here to help you simplify complexity and achieve success, whether through my platforms or courses.
 </p>
 </div>
