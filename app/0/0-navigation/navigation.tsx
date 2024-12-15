0622e4be625f65a0781457e160e9772abb10c287
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-15 17:14:34 -0500

    onclick

--- app/0/0-navigation/navigation.tsx
+++ app/0/0-navigation/navigation.tsx
@@ -25,13 +25,13 @@ const Navigation = () => {
       </Link>
       <nav className="nav">
         <Link href="/" className={`nav-link ${pathname === '/' ? 'active' : ''}`} onClick={handleLinkClick}>Home</Link>
-        <Link href="/2/about" className={`nav-link ${pathname === '/2/about' ? 'active' : ''}`} onClick={handleLinkClick}>About</Link>
-        <Link href="/2/videos" className={`nav-link ${pathname === '/2/videos' ? 'active' : ''}`} onClick={handleLinkClick}>Videos</Link>
-        <Link href="/2/podcast" className={`nav-link ${pathname === '/2/podcast' ? 'active' : ''}`} onClick={handleLinkClick}>Podcast</Link>
-        <Link href="/2/apps" className={`nav-link ${pathname === '/2/apps' ? 'active' : ''}`} onClick={handleLinkClick}>Apps</Link>
-        <Link href="/2/books" className={`nav-link ${pathname === '/2/books' ? 'active' : ''}`} onClick={handleLinkClick}>Books</Link>
-        <Link href="/3" className={`nav-link ${pathname === '/3' ? 'active' : ''}`} onClick={handleLinkClick}>Road Map</Link>
-        <Link href="/2/newsletter" className={`newsletter-button ${pathname === '/2/newsletter' ? 'active' : ''}`} onClick={handleLinkClick}>Newsletter</Link>
+        <Link href="/2/about" className={`nav-link ${pathname === '/2/about' ? 'active' : ''}`} >About</Link>
+        <Link href="/2/videos" className={`nav-link ${pathname === '/2/videos' ? 'active' : ''}`} >Videos</Link>
+        <Link href="/2/podcast" className={`nav-link ${pathname === '/2/podcast' ? 'active' : ''}`} >Podcast</Link>
+        <Link href="/2/apps" className={`nav-link ${pathname === '/2/apps' ? 'active' : ''}`} >Apps</Link>
+        <Link href="/2/books" className={`nav-link ${pathname === '/2/books' ? 'active' : ''}`} >Books</Link>
+        <Link href="/3" className={`nav-link ${pathname === '/3' ? 'active' : ''}`} >Road Map</Link>
+        <Link href="/2/newsletter" className={`newsletter-button ${pathname === '/2/newsletter' ? 'active' : ''}`} >Newsletter</Link>
       </nav>
     </header>
   );
