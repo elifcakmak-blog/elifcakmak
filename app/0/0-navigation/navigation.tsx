77e81aead88f24cb58c8288f01a3781e59e1fe30
Julianne Murdock <elifcakmak.blog@gmail.com>
2024-12-15 18:19:00 -0500

    nav refresh

--- app/0/0-navigation/navigation.tsx
+++ app/0/0-navigation/navigation.tsx
@@ -6,32 +6,22 @@ import { usePathname } from 'next/navigation';
 import './navigation.css';
 
 const Navigation = () => {
-  const pathname = usePathname();
-
-  // Function to trigger page reload after navigation
-  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    const href = e.currentTarget.getAttribute('href') || '/'; // Get the href attribute of the clicked link
-
-    // Delay page reload after navigation
-    setTimeout(() => {
-      window.location.href = href; // Navigate to the desired page
-    }, 0);
-  };
+  const pathname = usePathname(); 
 
   return (
-    <header className={`header ${pathname === '/3' ? 'highlight' : ''}`}>
-      <Link href="/" className="logo-link">
+    <header className="header">
+      <Link href="https://www.elifcakmak.blog/" className="logo-link">
         <Image src="/elif-logo/elifcakmaklogo.svg" alt="Elif Çakmak Logo" width={100} height={100} className="logo" />
       </Link>
       <nav className="nav">
-        <Link href="/" className={`nav-link ${pathname === '/' ? 'active' : ''}`} onClick={handleLinkClick}>Home</Link>
-        <Link href="/2/about" className={`nav-link ${pathname === '/2/about' ? 'active' : ''}`} >About</Link>
-        <Link href="/2/videos" className={`nav-link ${pathname === '/2/videos' ? 'active' : ''}`} >Videos</Link>
-        <Link href="/2/podcast" className={`nav-link ${pathname === '/2/podcast' ? 'active' : ''}`} >Podcast</Link>
-        <Link href="/2/apps" className={`nav-link ${pathname === '/2/apps' ? 'active' : ''}`} >Apps</Link>
-        <Link href="/2/books" className={`nav-link ${pathname === '/2/books' ? 'active' : ''}`} >Books</Link>
-        <Link href="/3" className={`nav-link ${pathname === '/3' ? 'active' : ''}`} >Road Map</Link>
-        <Link href="/2/newsletter" className={`newsletter-button ${pathname === '/2/newsletter' ? 'active' : ''}`} >Newsletter</Link>
+      <Link href="/" className={`nav-link ${pathname === '/' ? 'active' : ''}`}onClick={(e) => { e.preventDefault();window.location.href = "/";}}>Home</Link>
+        <Link href="/2/about" className={`nav-link ${pathname === '/2/about' ? 'active' : ''}`}>About</Link>
+        <Link href="/2/videos" className={`nav-link ${pathname === '/2/videos' ? 'active' : ''}`}>Videos</Link>
+        <Link href="/2/podcast" className={`nav-link ${pathname === '/2/podcast' ? 'active' : ''}`}>Podcast</Link>
+        <Link href="/2/apps" className={`nav-link ${pathname === '/2/apps' ? 'active' : ''}`}>Apps</Link>
+        <Link href="/2/books" className={`nav-link ${pathname === '/2/books' ? 'active' : ''}`}>Books</Link>
+        <Link href="/3" className={`nav-link ${pathname === '/3' ? 'active' : ''}`}>Road Map</Link>
+        <Link href="/2/newsletter" className={`newsletter-button ${pathname === '/2/newsletter' ? 'active' : ''}`}>Newsletter</Link>
       </nav>
     </header>
   );
