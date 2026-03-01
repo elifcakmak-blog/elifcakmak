c5cfbe5bfa8969ca4aa87a3ffea54a0d4076c4f6
Elif Çakmak <elifcakmak.blog@gmail.com>
2026-03-01 10:16:01 -0500

    Update March Roadmap

--- app/3-months/M2026/26-3/page.tsx
+++ app/3-months/M2026/26-3/page.tsx
@@ -4,7 +4,37 @@ import MilestonePage from '../../../3/Milestones/milestone';
 const App: React.FC = () => {
   const milestones = [
     {
-      id: 4,
+      id: 1,
+      imageSrc: '/roadmap-tiles/building.svg',
+      title: 'Code 2/2 Effortless Works Business Brand Website',
+      circles: [
+        { id: 1, description: 'Make the Courses Nav Pages',color: '#504081'},
+        { id: 2, description: 'Make the Info & Contact Page & Asthetics',color: '#504081'},
+        { id: 3, description: 'Home Page Asthetics',color: '#504081' },
+        { id: 4, description: 'Business Home Page Asthetics',color: '#504081'},
+        { id: 5, description: 'Individuals Home Page Asthetics',color: '#504081'},
+        { id: 6, description: 'Quest Pages Asthetics',color: '#504081'},
+        { id: 7, description: 'Courses Home Page Asthetics',color: '#504081'},
+        { id: 8, description: 'Business Sub Home Pages Asthetics',color: '#504081'},
+        { id: 9, description: 'Individuals Sub Home Pages Asthetics',color: '#504081'},
+        { id: 10, description: 'Courses Sub Home Pages Asthetics',color: '#504081'},
+        { id: 11, description: 'Contact Page Asthetics',color: '#504081'},
+        { id: 12, description: 'Footer + All Pages Final Touches',color: '#504081'},
+      ],
+    },
+    {
+      id: 2,
+      imageSrc: '/roadmap-tiles/nextt.svg',
+      title: 'Make Effortless Project Management Sheets (Business) Product Line',
+      circles: [
+        { id: 1, description: 'Make the Project Dashboard Page',color: '#504081'},
+        { id: 2, description: 'Make the Project Folder Set',color: '#504081'},
+        { id: 3, description: 'Make the Version Control Folder Set',color: '#504081'},
+        { id: 4, description: 'Make the Individual Branch Folder Set',color: '#504081'},
+      ],
+    },
+    {
+      id: 3,
       imageSrc: '/roadmap-tiles/nextt.svg',
       title: 'Make Effortless Back Office Sheets (Business) Product Line',
       circles: [
@@ -18,7 +48,7 @@ const App: React.FC = () => {
       ],
     },
     {
-      id: 5,
+      id: 4,
       imageSrc: '/roadmap-tiles/nextt.svg',
       title: 'Make 5 New Podcast Episodes',
       circles: [
@@ -30,7 +60,7 @@ const App: React.FC = () => {
       ],
     },
     {
-      id: 6,
+      id: 5,
       imageSrc: '/roadmap-tiles/nextt.svg',
       title: 'Make 10 Newsletters',
       circles: [
@@ -47,7 +77,7 @@ const App: React.FC = () => {
       ],
     },
     {
-      id: 7,
+      id: 6,
       imageSrc: '/roadmap-tiles/nextt.svg',
       title: 'Make 5 New Chapters for Book 1',
       circles: [
@@ -59,7 +89,7 @@ const App: React.FC = () => {
       ],
     },
     {
-      id: 8,
+      id: 7,
       imageSrc: '/roadmap-tiles/nextt.svg',
       title: 'Code Effortless Quest: Website Structure Skeleton',
       circles: [
@@ -75,86 +105,7 @@ const App: React.FC = () => {
         { id: 10, description: 'Log-in:  TownHall: Charity Center',color: '#504081'},
       ],
     },
-    {
-      id: 9,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make Effortless Back Office Notions (Business) Product Line',
-      circles: [
-        { id: 1, description: 'Make the Sales Tracking Page',color: '#504081'},
-        { id: 2, description: 'Make the KPI Tracking Page',color: '#504081'},
-        { id: 3, description: 'Make the Client Tracking Page',color: '#504081'},
-        { id: 4, description: 'Make the Commission Tracking Page',color: '#504081'},
-        { id: 5, description: 'Make the Payroll Tracking Page',color: '#504081'},
-        { id: 6, description: 'Make the Recruitment Tracking Page',color: '#504081'},
-        { id: 7, description: 'Make the None of these? Page',color: '#504081'},
-        { id: 8, description: 'Fully Complete Back Offfice ',color: '#504081'},
-      ],
-    },
-    {
-      id: 10,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make 4 New Podcast Episodes',
-      circles: [
-        { id: 1, description: 'Podcast Episode 1',color: '#504081' },
-        { id: 2, description: 'Podcast Episode 2',color: '#504081'},
-        { id: 3, description: 'Podcast Episode 3',color: '#504081'},
-        { id: 4, description: 'Podcast Episode 4',color: '#504081'},
-      ],
-    },
-    {
-      id: 11,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make 2 New Youtube Videos',
-      circles: [
-        { id: 1, description: 'Youtube Video 1',color: '#504081' },
-        { id: 2, description: 'Youtube Video 2',color: '#504081'},
-      ],
-    },
-    {
-      id: 12,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make 10 New Short Videos and Post to All Platforms',
-      circles: [
-        { id: 1, description: 'Short Video 1',color: '#504081' },
-        { id: 2, description: 'Short Video 2',color: '#504081' },
-        { id: 3, description: 'Short Video 3',color: '#504081' },
-        { id: 4, description: 'Short Video 4',color: '#504081' },
-        { id: 5, description: 'Short Video 5',color: '#504081' },
-        { id: 6, description: 'Short Video 6',color: '#504081' },
-        { id: 7, description: 'Short Video 7',color: '#504081' },
-        { id: 8, description: 'Short Video 8',color: '#504081' },
-        { id: 9, description: 'Short Video 9',color: '#504081' },
-        { id: 10, description: 'Short Video 10',color: '#504081' },
-      ],
-    },
-    {
-      id: 13,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make 10 Newsletters',
-      circles: [
-        { id: 1, description: 'Newsletter 1',color: '#504081' },
-        { id: 2, description: 'Newsletter 2',color: '#504081' },
-        { id: 3, description: 'Newsletter 3',color: '#504081' },
-        { id: 4, description: 'Newsletter 4',color: '#504081' },
-        { id: 5, description: 'Newsletter 5',color: '#504081' },
-        { id: 6, description: 'Newsletter 6',color: '#504081' },
-        { id: 7, description: 'Newsletter 7',color: '#504081' },
-        { id: 8, description: 'Newsletter 8',color: '#504081' },
-        { id: 9, description: 'Newsletter 9',color: '#504081' },
-        { id: 10, description: 'Newsletter 10',color: '#504081' },
-      ],
-    },
-    {
-      id: 14,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make 4 New Chapters for Book 1',
-      circles: [
-        { id: 1, description: 'Chapter 6',color: '#504081' },
-        { id: 2, description: 'Chapter 7',color: '#504081'},
-        { id: 3, description: 'Chapter 8',color: '#504081'},
-        { id: 4, description: 'Chapter 9',color: '#504081'},
-      ],
-    },
+    
   ];
 
   return <MilestonePage milestones={milestones} />;
--- app/3-months/M2026/26-7/page.tsx
+++ app/3-months/M2026/26-7/page.tsx
@@ -3,17 +3,7 @@ import MilestonePage from '../../../3/Milestones/milestone';
 
 const App: React.FC = () => {
   const milestones = [
-    {
-      id: 1,
-      imageSrc: '/roadmap-tiles/nextt.svg',
-      title: 'Make Effortless Project Management Sheets (Business) Product Line',
-      circles: [
-        { id: 1, description: 'Make the Project Dashboard Page',color: '#504081'},
-        { id: 2, description: 'Make the Project Folder Set',color: '#504081'},
-        { id: 3, description: 'Make the Version Control Folder Set',color: '#504081'},
-        { id: 4, description: 'Make the Individual Branch Folder Set',color: '#504081'},
-      ],
-    },
+    
     {
       id: 2,
       imageSrc: '/roadmap-tiles/nextt.svg',
