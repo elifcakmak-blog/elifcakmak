ba6f5af10b7f1f5759aa7b29254a7ba5f9e26604
Elif Çakmak <elifcakmak.blog@gmail.com>
2026-03-05 06:52:24 -0500

    update sıtemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://elifcakmak.blog", // Replace with your website URL
+    siteUrl: "https://www.muslimsuccesspath.com/", // Replace with your website URL
     generateRobotsTxt: true, // (Optional) Generate a `robots.txt` file
     sitemapSize: 5000, // Split sitemap into multiple files if there are more than 5000 URLs
     changefreq: "daily", // Update frequency for pages
